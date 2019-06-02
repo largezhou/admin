dd6ac8114156a9a5ef21fdb79b6343a79d188d9d
largezhou <largezhou@hotmail.com>
2019-06-03 07:45:22 +0800

    SwitchEdit 中,更新成功后,更新oldVal

--- resources/src/components/quick-edit/SwitchEdit.vue
+++ resources/src/components/quick-edit/SwitchEdit.vue
@@ -23,7 +23,7 @@ export default {
   },
   async mounted() {
     await this.$nextTick()
-    this.oldVal = this.$refs.switch.value
+    this.setOldVal()
   },
   methods: {
     async onChange(val) {
@@ -34,12 +34,16 @@ export default {
       try {
         this.loading = true
         await this.update(this.id, { [this.field]: val })
+        this.setOldVal()
         this.$message.success('修改成功')
       } catch (e) {
         this.$refs.switch.$emit('input', this.oldVal)
       } finally {
         this.loading = false
       }
+    },
+    setOldVal() {
+      this.oldVal = this.$refs.switch.value
     },
   },
 }
