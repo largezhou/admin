d3fa3b4bd314647d35656dfa1002424e9820640e
largezhou <largezhou@hotmail.com>
2019-06-12 21:19:27 +0800

    重写 FormItem, 使设置 required 之后, 只显示红星, 不验证表单

--- resources/src/plugins/element/components/FormItem.js
+++ resources/src/plugins/element/components/FormItem.js
@@ -1,4 +1,5 @@
 import { FormItem } from 'element-ui'
+import { getPropByPath } from 'element-ui/src/utils/util'
 
 FormItem.mounted = function () {
   if (this.prop) {
@@ -23,6 +24,16 @@ export default {
   methods: {
     setInitialValue() {
       this.initialValue = this.fieldValue
+    },
+    // form-item 设置 required 之后, 只会自动在前面加上 红色 * 号, 不会自动验证是否填了值
+    getRules() {
+      let formRules = this.form.rules
+      const selfRules = this.rules
+
+      const prop = getPropByPath(formRules, this.prop || '')
+      formRules = formRules ? (prop.o[this.prop || ''] || prop.v) : []
+
+      return [].concat(selfRules || formRules || [])
     },
   },
 }
