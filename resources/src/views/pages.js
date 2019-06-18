5b6ca43daa4766b6dbd1f9d6751062277bdb4ca4
largezhou <largezhou@hotmail.com>
2019-06-18 11:39:04 +0800

    动态路由组件, 不再定义 JS 分块文件名

--- resources/src/views/pages.js
+++ resources/src/views/pages.js
@@ -1,14 +1,14 @@
 export default {
   'index': () => import('@v/Index'),
-  'vue-routers/create': () => import(/* webpackChunkName: 'vur-routers' */ '@v/vue-routers/Form'),
-  'vue-routers': () => import(/* webpackChunkName: 'vur-routers' */ '@v/vue-routers/Index'),
-  'vue-routers/:id(\\d+)/edit': () => import(/* webpackChunkName: 'vur-routers' */ '@v/vue-routers/Form'),
+  'vue-routers/create': () => import('@v/vue-routers/Form'),
+  'vue-routers': () => import('@v/vue-routers/Index'),
+  'vue-routers/:id(\\d+)/edit': () => import('@v/vue-routers/Form'),
 
-  'admin-permissions': () => import(/* webpackChunkName: 'admin-permissions' */ '@v/admin-permissions/Index'),
-  'admin-permissions/create': () => import(/* webpackChunkName: 'admin-permissions' */ '@v/admin-permissions/Form'),
-  'admin-permissions/:id(\\d+)/edit': () => import(/* webpackChunkName: 'admin-permissions' */ '@v/admin-permissions/Form'),
+  'admin-permissions': () => import('@v/admin-permissions/Index'),
+  'admin-permissions/create': () => import('@v/admin-permissions/Form'),
+  'admin-permissions/:id(\\d+)/edit': () => import('@v/admin-permissions/Form'),
 
-  'admin-roles': () => import(/* webpackChunkName: 'admin-roles' */ '@v/admin-roles/Index'),
-  'admin-roles/create': () => import(/* webpackChunkName: 'admin-roles' */ '@v/admin-roles/Form'),
-  'admin-roles/:id(\\d+)/edit': () => import(/* webpackChunkName: 'admin-roles' */ '@v/admin-roles/Form'),
+  'admin-roles': () => import('@v/admin-roles/Index'),
+  'admin-roles/create': () => import('@v/admin-roles/Form'),
+  'admin-roles/:id(\\d+)/edit': () => import('@v/admin-roles/Form'),
 }
