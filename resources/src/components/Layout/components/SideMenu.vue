217c468a95aacbca38256c10838deb9bbac9a16b
largezhou <largezhou@hotmail.com>
2019-06-26 08:17:23 +0800

    完善侧边栏菜单的的自动展开

--- resources/src/components/Layout/components/SideMenu.vue
+++ resources/src/components/Layout/components/SideMenu.vue
@@ -1,10 +1,13 @@
 <template>
   <el-menu
     :default-active="activeName"
+    :default-openeds="openedMenus"
     class="side-menu"
     background-color="#304156"
     text-color="#bfcbd9"
     :collapse="collapse"
+    @close="onClose"
+    @open="onOpen"
   >
     <template v-for="menu of menus">
       <side-menu-item v-if="menu.menu" :menu="menu" :key="menu.id"/>
@@ -14,6 +17,7 @@
 
 <script>
 import SideMenuItem from './SideMenuItem'
+import _forIn from 'lodash/forIn'
 
 export default {
   name: 'SideMenu',
@@ -21,7 +25,9 @@ export default {
     SideMenuItem,
   },
   data() {
-    return {}
+    return {
+      uniqueOpenedMenus: {},
+    }
   },
   props: {
     collapse: Boolean,
@@ -32,6 +38,31 @@ export default {
     },
     activeName() {
       return this.$route.name
+    },
+    openedMenus() {
+      const res = []
+      _forIn(this.uniqueOpenedMenus, (val, key) => {
+        val && res.push(key)
+      })
+      return res
+    },
+  },
+  methods: {
+    onClose(index) {
+      this.$set(this.uniqueOpenedMenus, index, false)
+    },
+    onOpen(index) {
+      this.$set(this.uniqueOpenedMenus, index, true)
+    },
+  },
+  watch: {
+    $route: {
+      handler(newVal) {
+        newVal.matched.forEach(i => {
+          i.name && this.$set(this.uniqueOpenedMenus, i.name, true)
+        })
+      },
+      immediate: true,
     },
   },
 }
