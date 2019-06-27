e327ee472d0d668139484e091843f5c925783f12
largezhou <largezhou@hotmail.com>
2019-06-27 08:12:31 +0800

    侧边栏菜单，在小屏下，折叠后完全隐藏

--- resources/src/components/Layout/components/SideMenu.vue
+++ resources/src/components/Layout/components/SideMenu.vue
@@ -1,23 +1,29 @@
 <template>
-  <el-menu
-    :default-active="activeName"
-    :default-openeds="openedMenus"
-    class="side-menu"
-    background-color="#304156"
-    text-color="#bfcbd9"
-    :collapse="collapse"
-    @close="onClose"
-    @open="onOpen"
-  >
-    <template v-for="menu of menus">
-      <side-menu-item v-if="menu.menu" :menu="menu" :key="menu.id"/>
-    </template>
-  </el-menu>
+  <div style="height: 100%;">
+    <div v-if="miniWidth && !collapse" class="mask" @click="onCollapse"/>
+    <el-menu
+      :default-active="activeName"
+      :default-openeds="openedMenus"
+      class="side-menu"
+      :class="{ 'mini-width': miniWidth }"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      :collapse="collapse"
+      @close="onClose"
+      @open="onOpen"
+      @select="onSelect"
+    >
+      <template v-for="menu of menus">
+        <side-menu-item v-if="menu.menu" :menu="menu" :key="menu.id"/>
+      </template>
+    </el-menu>
+  </div>
 </template>
 
 <script>
 import SideMenuItem from './SideMenuItem'
 import _forIn from 'lodash/forIn'
+import { mapState } from 'vuex'
 
 export default {
   name: 'SideMenu',
@@ -46,6 +52,9 @@ export default {
       })
       return res
     },
+    ...mapState({
+      miniWidth: (state) => state.miniWidth,
+    }),
   },
   methods: {
     onClose(index) {
@@ -53,6 +62,12 @@ export default {
     },
     onOpen(index) {
       this.$set(this.uniqueOpenedMenus, index, true)
+    },
+    onCollapse() {
+      this.$store.commit('SET_OPENED', false)
+    },
+    onSelect() {
+      this.miniWidth && this.onCollapse()
     },
   },
   watch: {
@@ -70,10 +85,38 @@ export default {
 
 <style scoped lang="scss">
 .side-menu {
+  border: none;
   height: 100%;
 }
 
 .side-menu:not(.el-menu--collapse) {
   width: 220px;
+}
+
+.mini-width {
+  position: fixed;
+  z-index: 2001; // 比 页面的 loading 层高一点
+  left: 0;
+  top: 0;
+  bottom: 0;
+  overflow-y: auto;
+
+  &.el-menu--collapse {
+    width: 0;
+
+    > * {
+      display: none;
+    }
+  }
+}
+
+.mask {
+  background: #000;
+  opacity: .3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 2001;
 }
 </style>
