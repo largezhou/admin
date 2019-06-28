30936c863bccba106e25045d81af15422ee2cd73
largezhou <largezhou@hotmail.com>
2019-06-28 22:23:43 +0800

    路由配置时，全部去掉路径的前置斜杠

--- routes/admin.php
+++ routes/admin.php
@@ -6,16 +6,16 @@ Route::middleware([
     'auth:admin',
     'admin.permission',
 ])->group(function () {
-    Route::post('/auth/logout', 'Auth\LoginController@logout')->name('logout');
+    Route::post('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
-    Route::get('/user', 'AdminUserController@user')->name('user');
-    Route::get('/user/edit', 'AdminUserController@editUser')->name('user.edit');
-    Route::put('/user', 'AdminUserController@updateUser')->name('user.update');
+    Route::get('user', 'AdminUserController@user')->name('user');
+    Route::get('user/edit', 'AdminUserController@editUser')->name('user.edit');
+    Route::put('user', 'AdminUserController@updateUser')->name('user.update');
 
-    Route::resource('/admin-users', 'AdminUserController');
-    Route::resource('/vue-routers', 'VueRouterController')->except(['show', 'create']);
-    Route::resource('/admin-permissions', 'AdminPermissionController')->except(['show']);
-    Route::resource('/admin-roles', 'AdminRoleController')->except(['show']);
+    Route::resource('admin-users', 'AdminUserController');
+    Route::resource('vue-routers', 'VueRouterController')->except(['show', 'create']);
+    Route::resource('admin-permissions', 'AdminPermissionController')->except(['show']);
+    Route::resource('admin-roles', 'AdminRoleController')->except(['show']);
 
     Route::prefix('configs')
         ->as('configs.')
@@ -23,23 +23,23 @@ Route::middleware([
             Route::get('vue-routers', 'ConfigController@vueRouters')->name('vue-routers');
         });
 
-    Route::resource('/system-media-categories', 'SystemMediaCategoryController')->except(['show', 'create']);
+    Route::resource('system-media-categories', 'SystemMediaCategoryController')->except(['show', 'create']);
     // 在指定分类下，上传文件
     Route::post(
-        '/system-media-categories/{system_media_category}/system-media',
+        'system-media-categories/{system_media_category}/system-media',
         'SystemMediaCategoryController@storeSystemMedia'
     )->name('system-media-categories.system-media.store');
     // 获取指定分类下的所有文件
     Route::get(
-        '/system-media-categories/{system_media_category}/system-media',
+        'system-media-categories/{system_media_category}/system-media',
         'SystemMediaCategoryController@systemMediaIndex'
     )->name('system-media-categories.system-media.index');
 
-    Route::resource('/system-media', 'SystemMediaController')
+    Route::resource('system-media', 'SystemMediaController')
         // system-media 自动转成单数后 变为了 system-sedium
         // 所以手动指定
         ->parameters(['system-media' => 'system_media'])
         ->except(['store', 'show', 'create']);
 });
 
-Route::post('/auth/login', 'Auth\LoginController@login')->name('login');
+Route::post('auth/login', 'Auth\LoginController@login')->name('login');
