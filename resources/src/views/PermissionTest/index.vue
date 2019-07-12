bbec994a70cbdfd6d11f9c1a0ae3501ba80361f6
largezhou <largezhou@hotmail.com>
2019-07-12 10:25:51 +0800

    调整角色权限测试页面

--- resources/src/views/PermissionTest/index.vue
+++ resources/src/views/PermissionTest/index.vue
@@ -6,24 +6,26 @@
     <div class="mb-3">
       <p>角色</p>
       <items :items="roles"/>
+      <!--每次更新，都重新渲染该部分，可以重置指令-->
+      <div class="mt-3" :key="Date.now()">
+        <el-tag v-role-in="needRoles">有权限（{{ needRoles.join(',') }},administrator 有一个）</el-tag>
+        <el-tag v-role-in.not="needRoles">无权限（{{ needRoles.join(',') }},administrator 都没有）</el-tag>
+      </div>
     </div>
     <div>
       <p>权限</p>
       <items :items="perms"/>
-    </div>
-    <div class="test">
-      <p>权限角色测试</p>
-      <el-tag v-role-in="roleIn">administrator 或者有角色之一，可显示</el-tag>
-      <el-input v-model="roleIn"/>
-      <el-tag v-can="can">pass-all 或者权限都有，可显示</el-tag>
-      <el-input v-model="can"/>
+      <!--每次更新，都重新渲染该部分，可以重置指令-->
+      <div class="mt-3" :key="Date.now()">
+        <el-tag v-can="needPerms">有权限（{{ needPerms.join(',') }} 都有, 或者有 pass-all）</el-tag>
+        <el-tag v-can.not="needPerms">无权限（{{ needPerms.join(',') }} 有一个没有，且没有 pass-all）</el-tag>
+      </div>
     </div>
   </el-card>
 </template>
 
 <script>
 import Items from '@v/PermissionTest/Items'
-import _get from 'lodash/get'
 
 export default {
   name: 'PermissionTest',
@@ -32,17 +34,15 @@ export default {
   },
   data() {
     return {
-      roleIn: 'editor,manager',
-      can: 'edit-post,check-post',
+      needRoles: ['editor', 'manager'],
+      needPerms: ['edit-post', 'check-post'],
     }
   },
   computed: {
     roles() {
-      // return _get(this.$store, 'state.users.user.roles', [])
       return this.$store.getters.userInfo('roles', [])
     },
     perms() {
-      // return _get(this.$store, 'state.users.user.permissions', [])
       return this.$store.getters.userInfo('permissions', [])
     },
   },
