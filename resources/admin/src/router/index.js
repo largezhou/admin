99146c39bc53b5627ebc84d5ab699837d10e0e6a
largzhou <largezhou@hotmail.com>
2020-04-12 11:02:32 +0800

    小调整

--- resources/admin/src/router/index.js
+++ resources/admin/src/router/index.js
@@ -25,10 +25,8 @@ const router = new Router({
   base: process.env.NODE_ENV === 'development' ? 'admin-dev' : 'admin',
   routes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      Vue.nextTick(() => {
-        scrollEl && (scrollEl.scrollTop = savedPosition.y)
-      })
+    if (savedPosition && scrollEl) {
+      scrollEl.scrollTop = savedPosition.y
     }
   },
 })
